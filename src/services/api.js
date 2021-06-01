f0e0713f5044f39afc0facafb5b105a9d40aed63
luiizsilverio <luiiz.silverio@gmail.com>
2021-06-01 08:40:12 -0300

    Fix: exclusão de arquivos desnecessários

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-export const api = axios.create({
-	baseURL: 'http://localhost:3333'
+const api = axios.create({
+	//baseURL: 'http://localhost:3333'
+	baseURL: 'http://10.0.2.2:3333'
+	
 	//headers: { Authorization: `Bearer ${token}` }
 })
+
+export default api
